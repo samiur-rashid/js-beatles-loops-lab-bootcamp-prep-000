2e692f9acf4461f5ff4bff563c5d7907d7497961
Samiur Rashid <samrashid@gmail.com>
2017-10-16 03:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function iLoveTheBeatles(number) {
   var array = [];
   var i = 0;
   do {
-    array[i].push("I love the Beatles!");
+    array[i] = "I love the Beatles!";
     number++;
     i++;
   } while (number < 15)
