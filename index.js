9cff7998cf19a5471bce81d2e3e7e72ce7319a3e
Samiur Rashid <samrashid@gmail.com>
2017-10-16 03:30:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = [];
+  for (var i = 0; i < musicians.length; i++) {
+    phrases.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return phrases;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  var newFacts = []
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++
+  }
+  return newFacts
+}
